ecd782cc84ec8d4c9e9a4289e675cf692adc34a4
Josefa <jmendezg@udd.cl>
2021-08-23 23:48:07 -0400

    Desafio Terminado

--- 4_2-to_do_list/src/App.vue
+++ 4_2-to_do_list/src/App.vue
@@ -1,28 +1,98 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="lista">
+    <h1>Crear una nueva tarea</h1>
+    <div class="ingresarTarea">
+      <h2 class="tarea">Tarea</h2>
+      <input
+        type="text"
+        placeholder="Ingrese nueva tarea"
+        v-model="nuevaActividad"
+      />
+      <button @click="agregarNuevaActividad">Agregar</button>
+    </div>
+    <h2>Lista</h2>
+    <ul>
+      <li v-for="(actividad, $index) in actividades" :key="$index">
+        {{ actividad }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: () => ({
+    nuevaActividad: "",
+    actividades: [],
+  }),
+  methods: {
+    agregarNuevaActividad() {
+      this.actividades.push(this.nuevaActividad);
+      this.nuevaActividad = "";
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+h1,
+h2,
+ul {
+  font-family: "Brush Script MT", cursive;
+}
+
+.lista {
+  background-color: white;
+  width: 60%;
+  border: 10px #77b779 solid;
+  margin: 0 auto;
+}
+
+h1 {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 20px;
+  color: #f4b498;
+  margin: 0;
+}
+
+.tarea {
+  display: inline-block;
+  padding-right: 20px;
+  color: rgb(80, 80, 80);
+}
+
+.ingresarTarea {
+  text-align: center;
+}
+
+input {
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 2px solid rgb(55, 55, 55);
+  background: none;
+  color: rgb(80, 80, 80);
+  margin-right: 20px;
+  height: 30px;
+}
+
+input[type="text"]:focus {
+}
+
+button {
+  background-color: #f57d5f;
+  padding: 5px 20px;
+  border: none;
+  color: white;
+}
+
+h2 {
+  color: rgb(80, 80, 80);
+  text-align: center;
+}
+
+li {
+  color: rgb(80, 80, 80);
+  margin: 0 100px;
 }
 </style>
