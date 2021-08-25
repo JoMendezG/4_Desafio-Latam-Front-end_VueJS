a0c82a299a85ef18d6731dc1489837ef92a857bc
Josefa <jmendezg@udd.cl>
2021-08-24 22:02:56 -0400

    Fin de la clase

---
+++ 4_5-poke_guia/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <div>
+      <label for=""> Nombre: </label>
+      <input type="text" v-model="nombreDelPokemon" />
+      <button @click="clickBotonBuscar()">Buscar</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data: () => ({
+    nombreDelPokemon: "",
+  }),
+  methods: {
+    clickBotonBuscar() {
+      console.log("nombre del pokemon =>", this.nombreDelPokemon);
+    },
+  },
+  created() {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${this.nombreDelPokemon}`)
+      .then((response) => response.json())
+      // Aquí me traigo toda la DB de la api
+      .then((json) => console.log(json));
+  },
+};
+</script>
+
+<style></style>
