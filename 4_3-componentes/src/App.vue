6851c1319bec5eee87cb86f348f2b518a0c63c3b
Josefa <jmendezg@udd.cl>
2021-08-24 20:03:10 -0400

    Desaf{io terminado

--- 4_3-componentes/src/App.vue
+++ 4_3-componentes/src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <rickMorty_Component />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import rickMorty_Component from "./components/rickMorty_component.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    rickMorty_Component,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-image: url(../public/background.png);
 }
 </style>
---
+++ 4_3-componentes/src/components/rickMorty_component.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(personaje, $index) in personajes" :key="$index">
+        <img class="imgPersonaje" alt="" v-bind:src="personaje.src" />
+        <h1 class="nombrePersonaje">{{ personaje.nombre }}</h1>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "rickMorty_Component",
+  data: () => ({
+    personajes: [
+      {
+        nombre: "Rick Sanchez",
+        src: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+      },
+      {
+        nombre: "Morty Smith",
+        src: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+      },
+      {
+        nombre: "Summer Smith",
+        src: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+      },
+      {
+        nombre: "Beth Smith",
+        src: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+      },
+      {
+        nombre: "Jerry Smith",
+        src: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+      },
+    ],
+  }),
+};
+</script>
+
+<style>
+.imgPersonaje {
+  border-radius: 50%;
+  width: 15%;
+  display: inline-block;
+  margin: 15px 0;
+}
+
+.nombrePersonaje {
+  color: greenyellow;
+  font-family: Arial, sans-serif;
+  font-weight: lighter;
+  display: inline-block;
+  position: absolute;
+  margin: 50px 20px;
+}
+</style>
