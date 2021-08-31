a5383c44609b60e59cb1a13e1120ea804238ee44
Josefa <jmendezg@udd.cl>
2021-08-31 10:41:02 -0400

    Desafio ramndom gift cat

--- 4_5-poke_guia/src/App.vue
+++ 4_5-poke_guia/src/App.vue
@@ -13,18 +13,28 @@ export default {
   name: "App",
   data: () => ({
     nombreDelPokemon: "",
+    guardarPokemon: this.guardarPokemon = datosDelPokemon
   }),
   methods: {
     clickBotonBuscar() {
       console.log("nombre del pokemon =>", this.nombreDelPokemon);
     },
+    pokemon_name() {
+      return this.datosDelPokemon.name;
+    },
+    callApi() {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${this.nombreDelPokemon}`)
+        // Asi me trae los 1118 pokemones y no 20
+        .then((response) => {
+          return response.json();
+        })
+        // Aqui puedo visualizar los 1118 pokemones en la consola
+        .then((datosDelPokemon) => {
+          console.log({ listaPokemones: datosDelPokemon });
+        });
+    },
   },
-  created() {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${this.nombreDelPokemon}`)
-      .then((response) => response.json())
-      // Aquí me traigo toda la DB de la api
-      .then((json) => console.log(json));
-  },
+  created() {},
 };
 </script>
 
