ef05f36b432dce6e883a2cc5ebdbeb3ae57a51f8
Josefa <jmendezg@udd.cl>
2021-08-31 22:03:47 -0400

    Fin avances desafío hoy

---
+++ 4_7-implementando_vue_router/src/components/Post.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <router-link :to="paginar(true)">Siguiente</router-link>
+    <div>
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- 4_7-implementando_vue_router/src/router.js
+++ 4_7-implementando_vue_router/src/router.js
@@ -3,6 +3,9 @@ import Router from "vue-router";
 import Portada from "./components/Portada.vue";
 import SobreMi from "./components/SobreMi.vue";
 import Contacto from "./components/Contacto.vue";
+import NotFound from "./components/NotFound.vue";
+import Post from "./components/Post.vue";
+import Articulo from "./components/Articulo.vue";
 
 Vue.use(Router);
 
@@ -10,7 +13,7 @@ export default new Router({
   mode: "history",
   routes: [
     {
-      path: "/.",
+      path: "/",
       component: Portada,
       name: "Portada",
     },
@@ -23,6 +26,22 @@ export default new Router({
       path: "/contacto",
       component: Contacto,
       name: "Contacto",
+    },
+    {
+      path: "/post",
+      component: Post,
+      children: [
+        {
+          path: "1",
+          component: Articulo,
+          name: "Articulo1",
+        },
+      ],
+    },
+    {
+      path: "/*",
+      component: NotFound,
+      name: "NotFound",
     },
   ],
 });
