d9dc878921780d9268d61687bf0200bdaedb7e4e
Josefa <jmendezg@udd.cl>
2021-09-19 04:28:43 -0300

    pokeguiaCompleto

--- 4_5-poke_guia/src/App.vue
+++ 4_5-poke_guia/src/App.vue
@@ -1,9 +1,31 @@
 <template>
-  <div id="app">
-    <div>
-      <label for=""> Nombre: </label>
-      <input type="text" v-model="nombreDelPokemon" />
-      <button @click="clickBotonBuscar()">Buscar</button>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col text-center p-5">
+        <img src="./assets/pokedex.png" alt="" class="logo" />
+        <label>Nombre:</label>
+        <input type="text" placeholder="Pikachu" v-model="nuevoPokemon" />
+        <button class="btn btn-primary" @click="getPokemonData">Buscar</button>
+      </div>
+    </div>
+    <div class="row text-center">
+      <div class="col">
+        <img :src="pokemonData.sprites.front_default" :alt="pokemonData.name" />
+      </div>
+    </div>
+    <div class="row text-center">
+      <div class="col">
+        <h2>Movimientos</h2>
+        <p>
+          {{ moves }}
+        </p>
+      </div>
+    </div>
+    <div class="row text-center">
+      <div class="col">
+        <h2>Habilidades</h2>
+        <p>{{ abilities }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -12,30 +34,59 @@
 export default {
   name: "App",
   data: () => ({
-    nombreDelPokemon: "",
-    guardarPokemon: this.guardarPokemon = datosDelPokemon
+    nuevoPokemon: "pikachu",
+    pokemonData: [],
   }),
+  created() {
+    this.getPokemonData();
+  },
   methods: {
-    clickBotonBuscar() {
-      console.log("nombre del pokemon =>", this.nombreDelPokemon);
-    },
-    pokemon_name() {
-      return this.datosDelPokemon.name;
-    },
-    callApi() {
-      fetch(`https://pokeapi.co/api/v2/pokemon/${this.nombreDelPokemon}`)
-        // Asi me trae los 1118 pokemones y no 20
-        .then((response) => {
-          return response.json();
-        })
-        // Aqui puedo visualizar los 1118 pokemones en la consola
-        .then((datosDelPokemon) => {
-          console.log({ listaPokemones: datosDelPokemon });
-        });
+    getPokemonData() {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${this.nuevoPokemon}`)
+        .then((response) => response.json())
+        .then((data) => (this.pokemonData = data));
     },
   },
-  created() {},
+  computed: {
+    imgPokemon() {
+      let image = this.pokemonData.sprites.front_default;
+      return image;
+    },
+    moves() {
+      let moves = this.pokemonData.moves
+        .map((move) => move.move.name)
+        .join(", ");
+      return moves;
+    },
+    abilities() {
+      let abilities = this.pokemonData.abilities
+        .map((ability) => ability.ability.name)
+        .join(", ");
+      return abilities;
+    },
+  },
 };
 </script>
 
-<style></style>
+<style>
+.logo {
+  display: block;
+  margin: 0 auto;
+}
+
+label {
+  margin-top: 50px;
+}
+
+.logo {
+  width: 40%;
+}
+.titulo {
+  font-size: 3rem;
+}
+
+img {
+  margin-top: -50px;
+  width: 300px;
+}
+</style>
