0427b571c38b48cbae13dbefd3e8462206174542
Josefa <jmendezg@udd.cl>
2021-11-30 23:37:32 -0300

    Desaf{io 2 y 3 de vue

---
+++ G15/4_2-to_do_list/src/Ejemplos.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <img :src="src" />
+
+    <!-- <h1 v-if="show">{{ msg }}</h1> // Trabaja con display
+    <p v-else>ELSE</p> -->
+
+    <!-- <h1 v-show="show">{{ msg }}</h1> -->
+    <!-- Este trabaja con visibility -->
+
+    <componente></componente>
+
+    <!-- <button @click="saludar">Click</button> -->
+
+    <!-- <ul>
+      <li v-for="(item, index) in lista" :key="index">{{ item }}</li>
+    </ul> -->
+
+    <!-- <h1 class="title" :class="{ red }">Esto es un título</h1>
+    <button @click="toggleRed">Click</button> -->
+
+    <input type="text" v-model="input" @keydown.enter="saludar()" />
+    <p>Resultado: {{ uppercase }}</p>
+
+    <a href="#" @click.prevent="saludar">Boton</a>
+  </div>
+</template>s
+
+<script>
+import Componente from "./components/Componente.vue";
+
+export default {
+  components: { Componente },
+  data() {
+    return {
+      lista: [1, 2, 3, 4],
+      show: false,
+      msg: "HOLA",
+      red: false,
+      input: "",
+      formulario: {
+        name: "",
+        rut: "",
+      },
+      src:
+        "https://desafiolatam.com/assets/home/logo-academia-bla-790873cdf66b0e681dfbe640ace8a602f5330bec301c409744c358330e823ae3.png",
+    };
+  },
+
+  computed: {
+    uppercase() {
+      return this.input.toUpperCase();
+    },
+  },
+
+  methods: {
+    saludar() {
+      alert("Hola");
+    },
+
+    toggleRed() {
+      this.red = !this.red;
+    },
+  },
+};
+</script>
+
+<style>
+.red {
+  color: red;
+}
+
+/* .blue {
+  color: blue;
+} */
+
+.title {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
---
+++ G15/4_2-to_do_list/src/components/ToDo.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="container">
+    <h1>Crea una nueva tarea</h1>
+    <div>
+      <fieldset>
+        <label for="task">Tarea</label>
+        <input type="text" name="task" v-model="task" @keydown.enter="insert" />
+        <button @click="insert">Crear</button>
+      </fieldset>
+    </div>
+    <div>
+      <h2>Lista</h2>
+      <ul class="tasks">
+        <!-- For para realizar la lista el index + 1 es para la enumeración -->
+        <li v-for="(item, index) in list" :key="index">
+          {{ index + 1 }}: {{ item }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      task: "",
+      list: [],
+    };
+  },
+  methods: {
+    insert() {
+      this.list.push(this.task);
+      this.task = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  width: 100%;
+  max-width: 400px;
+  margin: 1em auto;
+  font-family: sans-serif;
+  background-color: #e1d8fc;
+  padding: 30px 150px;
+}
+label {
+  color: #2c155b;
+  font-weight: 3800;
+  margin-right: 0.3em;
+}
+input {
+  border-radius: 5px;
+  border: none;
+}
+button {
+  background-color: #2c155b;
+  color: white;
+  border-radius: 50px;
+  border: none;
+  padding: 5px 15px;
+  margin-left: 0.3em;
+}
+h1 {
+  color: #2c155b;
+  text-align: center;
+}
+h2 {
+  color: #2c155b;
+  text-align: center;
+}
+ul {
+  max-width: 250px;
+  margin: 0 auto;
+}
+li {
+  font-weight: 700;
+}
+fieldset {
+  border: 0;
+  width: 70%;
+  display: flex;
+  margin: auto;
+  justify-content: space-around;
+}
+
+li {
+  color: #2c155b;
+  font-weight: 200;
+}
+</style>
