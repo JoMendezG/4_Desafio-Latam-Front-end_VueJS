396898f376d3b4d58abfb815883b7766dad1ce21
Josefa <jmendezg@udd.cl>
2021-12-03 01:36:14 -0300

    Desafío listo

--- G15/4_4-trabajando_con_props/src/components/compHijo.vue
+++ G15/4_4-trabajando_con_props/src/components/compHijo.vue
@@ -5,10 +5,15 @@
     <ul>
       <!-- * v-for requiere una sintaxis especial en forma de `item in items`, donde los items son la matriz de datos de origen y el item es un ALIAS para el elemento de matriz que se está iterando -->
       <!-- ** En palabras simples itemTarea es solo un nombre que se debe utilizar dentro de {{}} y se pone in tareas porque es la prop que nos trae el padre con la tarea a agregar -->
-      <li v-for="(itemTarea, index) in tareas" :key="index">
+      <li
+        class="listadoTareas"
+        v-for="(itemTarea, index) in tareas"
+        :key="index"
+      >
         <!-- 15. Añadiremos al lado del texto de la tarea un botón que nos permitirá eliminar la tarea -->
         <!-- 16. Creamos la función eliminarTarea que se activa al hacer click en el botón -->
-        {{ itemTarea }} <button @click="eliminarTarea(index)">Eliminar</button>
+        <button class="btnDelete" @click="eliminarTarea(index)">X</button>
+        {{ itemTarea }}
       </li>
     </ul>
   </div>
@@ -29,4 +34,24 @@ export default {
 };
 </script>
 
-<style></style>
+<style scope>
+ul {
+  display: block;
+  margin: 0 auto;
+  width: 35%;
+  list-style: none;
+  padding-left: 0px;
+}
+
+.btnDelete {
+  margin: 5px 0;
+  margin-right: 25px;
+  display: inline-block;
+  width: 40px;
+  background-color: #f5a580;
+}
+
+.btnDelete:hover {
+  background-color: #fab899;
+}
+</style>
--- G15/4_4-trabajando_con_props/src/components/compPadre.vue
+++ G15/4_4-trabajando_con_props/src/components/compPadre.vue
@@ -1,17 +1,18 @@
 <template>
-  <div>
+  <div class="formulario">
     <!-- 1. Creamos el html base del desafío-->
     <!-- 2. Cramos la variable que no permite guardar el texto o información de la tarea a agregar a través del v-model en el input-->
-    <h2>Tareas</h2>
+    <h1>Tareas Pendientes:</h1>
     <input
       type="text"
-      placeholder="Nueva tarea"
+      placeholder="Ingresa tu nueva tarea"
       v-model="nuevaTarea"
       @keydown.enter="agregarNuevaTarea"
+      maxlength="50"
     />
     <!-- 4. Creamos la función agregarNuevaTarea en el botón que se ejecutará cuando hago click -->
     <!-- ***** Agregamos un  @keydown.enter para agregar la tarea automáticamente al apretar el enter y saltando el botón-->
-    <button @click="agregarNuevaTarea">Agregar</button>
+    <button @click="agregarNuevaTarea">Agregar tarea</button>
     <!-- 8. Reflejamos la lista a trvés del componente hijo -->
     <!-- 11. Con : conectamos con el js tareas y así comunicamos con el props enviado al hijo que es este componente -->
     <!-- 19. Llamamos a eliminarTarea de la función creada recientemente y creammos un método nuevo-->
@@ -55,4 +56,41 @@ export default {
 };
 </script>
 
-<style></style>
+<style scope>
+.formulario {
+  padding: 50px 0;
+}
+
+h1 {
+  display: block;
+  text-align: center;
+  color: #413a73;
+  font-family: "Roboto", sans-serif;
+}
+
+input {
+  display: block;
+  margin: 0 auto;
+  width: 35%;
+  text-align: center;
+  border-radius: 50px;
+  border: 1px rgb(189, 189, 189) solid;
+  height: 36px;
+}
+
+button {
+  background-color: #413a73;
+  border-radius: 50px;
+  padding: 10px 0;
+  width: 35%;
+  color: white;
+  border: none;
+  margin: 25px auto;
+  display: block;
+  box-shadow: 0px 8px 25px rgba(0, 0, 0, 0.3);
+}
+
+button:hover {
+  background-color: #574e9d;
+}
+</style>
