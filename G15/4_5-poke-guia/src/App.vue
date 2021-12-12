a3446b44c0c19ff673e1c78e235430310b01082b
Josefa <jmendezg@udd.cl>
2021-12-12 05:07:56 -0300

    Pokeguia terminado!

---
+++ G15/4_5-poke-guia/src/App.vue
@@ -0,0 +1,233 @@
+<template>
+  <div class="contenido">
+    <div class="izquierda">
+      <img src="./assets/img/logo.png" alt="" />
+      <h2 class="tPokedex">Busca tu pokemon favorito</h2>
+      <!-- 10. Creamos el formulario que permitirá cambiar de pokemon -->
+      <!-- 11. Em el input aplicamos que el enter cargue la api nuevamente y llame a la variable namePokemon a cambiar -->
+      <input @keydown.enter="cargarApi" v-model="namePokemon" type="text" />
+      <button @click="cargarApi">Buscar pokemon</button>
+    </div>
+    <div class="derecha">
+      <!-- 7. Cargo el nombre del pokemon -->
+      <h2 class="namePokemon">{{ namePokemon }}</h2>
+
+      <ul class="typeListUl">
+        <!-- 9. Creo u v-for que elistará las clases del pokemon REPITO ESTO CON MOVIMIENTOS Y HABILIDADES-->
+        <li class="typeList" v-for="(type, index) in pokeType" :key="index">
+          {{ type.type.name }}
+        </li>
+      </ul>
+
+      <!-- 6. Cargamos la foto del pokemon según la función  pokePicture-->
+      <div class="imgPokemon">
+        <img :src="pokePicture" alt="" />
+      </div>
+
+      <div class="mov_hab">
+        <div class="Movimientos">
+          <h3>Movimeintos:</h3>
+          <ul class="ulMovimientos">
+            <li v-for="(mov, index) in movPokemon" :key="index">
+              {{ mov.move.name }}
+            </li>
+          </ul>
+        </div>
+
+        <div class="Habilidades">
+          <h3>Habilidaes:</h3>
+          <ul class="ulHabilidades">
+            <li v-for="(hab, index) in habPokemon" :key="index">
+              {{ hab.ability.name }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      namePokemon: "pikachu",
+      pokedata: {},
+    };
+  },
+  methods: {
+    // 1. Cargamos la api
+    cargarApi() {
+      // 2. Con fetch llamamos la api con una variable nombrePokemon para buscar al pokemon específico
+      fetch(`https://pokeapi.co/api/v2/pokemon/${this.namePokemon}`)
+        .then((response) => response.json())
+        .then((data) => {
+          // 3. Utilizamos la variable pokedata que almacenara la data o info de toda la api
+          this.pokedata = data;
+        });
+    },
+  },
+  created() {
+    // 4. Cargamos nuestra función que llama a la api
+    this.cargarApi();
+  },
+  computed: {
+    // 5. Creamos nuestra función que cargará la foto del pokemon
+    pokePicture() {
+      // Sí pokedata NO encuentra la propiedad sprietes retorna vacio
+      if (!Object.prototype.hasOwnProperty.call(this.pokedata, "sprites")) {
+        return;
+      }
+
+      return this.pokedata.sprites.front_default;
+    },
+
+    // 8. Creo la función que llamá al tipo de pokemon
+    pokeType() {
+      if (!Object.prototype.hasOwnProperty.call(this.pokedata, "types")) {
+        return;
+      }
+
+      return this.pokedata.types;
+    },
+
+    movPokemon() {
+      if (!Object.prototype.hasOwnProperty.call(this.pokedata, "moves")) {
+        return;
+      }
+
+      return this.pokedata.moves;
+    },
+
+    habPokemon() {
+      if (!Object.prototype.hasOwnProperty.call(this.pokedata, "abilities")) {
+        return;
+      }
+
+      return this.pokedata.abilities;
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  font-family: "Roboto", sans-serif;
+}
+
+.contenido {
+  display: flex;
+  justify-content: center;
+}
+
+.izquierda {
+  width: 40%;
+  margin-top: 50px;
+  background-color: green;
+}
+
+.derecha {
+  background-color: red;
+  width: 40%;
+  margin-top: 50px;
+}
+
+.namePokemon {
+  color: white;
+  margin: 30px;
+}
+
+.imgPokemon {
+  display: block;
+  margin: 0 auto;
+  width: 200px;
+  height: 200px;
+}
+
+.imgPokemon img {
+  display: block;
+  margin: 0 auto;
+  margin-top: 80px;
+  transform: scale(5);
+  transform: translate(100px 0);
+}
+
+.typeList {
+  display: inline-block;
+  margin-right: 20px;
+  padding: 0;
+  color: white;
+  background-color: #ffffff48;
+  padding: 8px 15px;
+  border-radius: 50px;
+}
+
+.typeListUl {
+  padding: 0px;
+  margin-left: 30px;
+}
+
+.mov_hab {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 0 10px;
+}
+
+.Movimientos,
+.Habilidades {
+  color: white;
+  width: 50%;
+}
+
+.ulMovimientos,
+.ulHabilidades {
+  background-color: #ffffff48;
+  border-radius: 10px;
+  margin: 20px 15px;
+  list-style: none;
+  padding: 10px 30px;
+  overflow: auto;
+  height: 150px;
+}
+
+.ulHabilidades {
+  margin-right: 30px;
+}
+
+.Movimientos h3,
+.Habilidades h3 {
+  padding-left: 20px;
+}
+
+.izquierda img {
+  width: 80%;
+  display: block;
+  margin: 0 auto;
+}
+
+.tPokedex {
+  text-align: center;
+  color: white;
+  font-size: 28px;
+  font-weight: 100;
+}
+
+input {
+  width: 80%;
+  display: block;
+  margin: 0 auto;
+  height: 36px;
+  border-radius: 36px;
+  border: none;
+  background-color: #ffffffa1;
+}
+
+button {
+  width: 80%;
+  height: 36px;
+  border-radius: 50px;
+  margin: 25px auto;
+  display: block;
+}
+</style>
