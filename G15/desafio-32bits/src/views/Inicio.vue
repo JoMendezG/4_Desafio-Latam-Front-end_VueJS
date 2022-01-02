16844086b75a4a35c2f4236b190fe815976cd604
Josefa <jmendezg@udd.cl>
2022-01-02 03:38:47 -0300

    Desafio VUEX 2

---
+++ G15/desafio-32bits/src/views/Inicio.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="titulos">
+    <h1>32bits</h1>
+    <h2>Juegos de PC y consolas</h2>
+
+    <p>Profe es lo mismo del sandbox no se moleste mucho en corregir :(</p>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+.titulos {
+  text-align: center;
+  margin-top: 10vh;
+}
+</style>
