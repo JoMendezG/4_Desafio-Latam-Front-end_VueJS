45d558fce0df87b2bcf74b59f4754e892dd61545
Josefa <jmendezg@udd.cl>
2022-01-02 03:51:03 -0300

    Vuex 1

---
+++ G15/4_9-vuex-1/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h3>
+      Hola profe esto es un proyecto vacío no se moleste en corregir solo
+      entregue para cumplir.
+    </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
